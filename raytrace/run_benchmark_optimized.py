5d34c1d8b2080ee6be4d40b799d28d9ec455e49f
root <gmichaq@gmail.com>
2026-09-21 05:32:29 +0000

    Change name of optimized benchmark

---
+++ raytrace/run_benchmark_optimized.py
@@ -0,0 +1,431 @@
+"""
+This file contains definitions for a simple raytracer.
+Copyright Callum and Tony Garnock-Jones, 2008.
+
+This file may be freely redistributed under the MIT license,
+http://www.opensource.org/licenses/mit-license.php
+
+From http://www.lshift.net/blog/2008/10/29/toy-raytracer-in-python
+"""
+
+import array
+import math
+
+import pyperf
+
+
+DEFAULT_WIDTH = 100
+DEFAULT_HEIGHT = 100
+EPSILON = 0.00001
+
+
+class Vector(object):
+
+    def __init__(self, initx, inity, initz):
+        self.x = initx
+        self.y = inity
+        self.z = initz
+
+    def __str__(self):
+        return '(%s,%s,%s)' % (self.x, self.y, self.z)
+
+    def __repr__(self):
+        return 'Vector(%s,%s,%s)' % (self.x, self.y, self.z)
+
+    def magnitude(self):
+        x, y, z = self.x, self.y, self.z
+        return math.sqrt(x * x + y * y + z * z)
+
+    def __add__(self, other):
+        # Flattened type check: assume Vector + Vector in the hot path
+        if type(other) is Point:
+            return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def scale(self, factor):
+        return Vector(factor * self.x, factor * self.y, factor * self.z)
+
+    def dot(self, other):
+        return (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
+
+    def cross(self, other):
+        x1, y1, z1 = self.x, self.y, self.z
+        x2, y2, z2 = other.x, other.y, other.z
+        return Vector(y1 * z2 - z1 * y2,
+                      z1 * x2 - x1 * z2,
+                      x1 * y2 - y1 * x2)
+
+    def normalized(self):
+        x, y, z = self.x, self.y, self.z
+        inv_mag = 1.0 / math.sqrt(x * x + y * y + z * z)
+        return Vector(x * inv_mag, y * inv_mag, z * inv_mag)
+
+    def negated(self):
+        return Vector(-self.x, -self.y, -self.z)
+
+    def __eq__(self, other):
+        return (self.x == other.x) and (self.y == other.y) and (self.z == other.z)
+
+    def isVector(self):
+        return True
+
+    def isPoint(self):
+        return False
+
+    def mustBeVector(self):
+        return self
+
+    def mustBePoint(self):
+        raise Exception('Vectors are not points!')
+
+    def reflectThrough(self, normal):
+        x, y, z = self.x, self.y, self.z
+        nx, ny, nz = normal.x, normal.y, normal.z
+        d2 = 2.0 * (x * nx + y * ny + z * nz)
+        return Vector(x - d2 * nx, y - d2 * ny, z - d2 * nz)
+
+
+Vector.ZERO = Vector(0, 0, 0)
+Vector.RIGHT = Vector(1, 0, 0)
+Vector.UP = Vector(0, 1, 0)
+Vector.OUT = Vector(0, 0, 1)
+
+assert Vector.RIGHT.reflectThrough(Vector.UP) == Vector.RIGHT
+assert Vector(-1, -1, 0).reflectThrough(Vector.UP) == Vector(-1, 1, 0)
+
+
+class Point(object):
+
+    def __init__(self, initx, inity, initz):
+        self.x = initx
+        self.y = inity
+        self.z = initz
+
+    def __str__(self):
+        return '(%s,%s,%s)' % (self.x, self.y, self.z)
+
+    def __repr__(self):
+        return 'Point(%s,%s,%s)' % (self.x, self.y, self.z)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        if type(other) is Point:
+            return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+        return Point(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def isVector(self):
+        return False
+
+    def isPoint(self):
+        return True
+
+    def mustBeVector(self):
+        raise Exception('Points are not vectors!')
+
+    def mustBePoint(self):
+        return self
+    
+
+class Sphere(object):
+
+    def __init__(self, centre, radius):
+        self.centre = centre
+        self.radius = radius
+
+    def __repr__(self):
+        return 'Sphere(%s,%s)' % (repr(self.centre), self.radius)
+
+    def intersectionTime(self, ray):
+        # Extract primitive coordinates to do raw math
+        cx, cy, cz = self.centre.x, self.centre.y, self.centre.z
+        rpx, rpy, rpz = ray.point.x, ray.point.y, ray.point.z
+        rvx, rvy, rvz = ray.vector.x, ray.vector.y, ray.vector.z
+
+        # cp = self.centre - ray.point
+        cpx, cpy, cpz = cx - rpx, cy - rpy, cz - rpz
+
+        # v = cp.dot(ray.vector)
+        v = cpx * rvx + cpy * rvy + cpz * rvz
+
+        # cp.dot(cp)
+        cp_sq = cpx * cpx + cpy * cpy + cpz * cpz
+
+        discriminant = (self.radius * self.radius) - (cp_sq - v * v)
+        if discriminant < 0:
+            return None
+        return v - math.sqrt(discriminant)
+
+    def normalAt(self, p):
+        # Inlined (p - self.centre).normalized()
+        x = p.x - self.centre.x
+        y = p.y - self.centre.y
+        z = p.z - self.centre.z
+        inv_mag = 1.0 / math.sqrt(x * x + y * y + z * z)
+        return Vector(x * inv_mag, y * inv_mag, z * inv_mag)
+    
+
+class Halfspace(object):
+
+    def __init__(self, point, normal):
+        self.point = point
+        self.normal = normal.normalized()
+
+    def __repr__(self):
+        return 'Halfspace(%s,%s)' % (repr(self.point), repr(self.normal))
+
+    def intersectionTime(self, ray):
+        # Inlined ray.vector.dot(self.normal)
+        v = (ray.vector.x * self.normal.x +
+             ray.vector.y * self.normal.y +
+             ray.vector.z * self.normal.z)
+        if v:
+            return 1.0 / -v
+        return None
+
+    def normalAt(self, p):
+        return self.normal
+
+
+class Ray(object):
+
+    def __init__(self, point, vector):
+        self.point = point
+        self.vector = vector.normalized()
+
+    def __repr__(self):
+        return 'Ray(%s,%s)' % (repr(self.point), repr(self.vector))
+
+    def pointAtTime(self, t):
+        # Inlined self.point + self.vector.scale(t)
+        return Point(self.point.x + self.vector.x * t,
+                     self.point.y + self.vector.y * t,
+                     self.point.z + self.vector.z * t)    
+
+
+Point.ZERO = Point(0, 0, 0)
+
+
+class Canvas(object):
+
+    def __init__(self, width, height):
+        self.bytes = array.array('B', [0] * (width * height * 3))
+        for i in range(width * height):
+            self.bytes[i * 3 + 2] = 255
+        self.width = width
+        self.height = height
+
+    def plot(self, x, y, r, g, b):
+        i = ((self.height - y - 1) * self.width + x) * 3
+        self.bytes[i] = max(0, min(255, int(r * 255)))
+        self.bytes[i + 1] = max(0, min(255, int(g * 255)))
+        self.bytes[i + 2] = max(0, min(255, int(b * 255)))
+
+    def write_ppm(self, filename):
+        header = 'P6 %d %d 255\n' % (self.width, self.height)
+        with open(filename, "wb") as fp:
+            fp.write(header.encode('ascii'))
+            fp.write(self.bytes.tobytes())
+
+
+def firstIntersection(intersections):
+    result = None
+    for i in intersections:
+        candidateT = i[1]
+        if candidateT is not None and candidateT > -EPSILON:
+            if result is None or candidateT < result[1]:
+                result = i
+    return result
+
+
+class Scene(object):
+
+    def __init__(self):
+        self.objects = []
+        self.lightPoints = []
+        self.position = Point(0, 1.8, 10)
+        self.lookingAt = Point.ZERO
+        self.fieldOfView = 45
+        self.recursionDepth = 0
+
+    def moveTo(self, p):
+        self.position = p
+
+    def lookAt(self, p):
+        self.lookingAt = p
+
+    def addObject(self, object, surface):
+        self.objects.append((object, surface))
+
+    def addLight(self, p):
+        self.lightPoints.append(p)
+
+    def render(self, canvas):
+        fovRadians = math.pi * (self.fieldOfView / 2.0) / 180.0
+        halfWidth = math.tan(fovRadians)
+        halfHeight = 0.75 * halfWidth
+        width = halfWidth * 2
+        height = halfHeight * 2
+        pixelWidth = width / (canvas.width - 1)
+        pixelHeight = height / (canvas.height - 1)
+
+        eye = Ray(self.position, self.lookingAt - self.position)
+        vpRight = eye.vector.cross(Vector.UP).normalized()
+        vpUp = vpRight.cross(eye.vector).normalized()
+
+        for y in range(canvas.height):
+            for x in range(canvas.width):
+                xcomp = vpRight.scale(x * pixelWidth - halfWidth)
+                ycomp = vpUp.scale(y * pixelHeight - halfHeight)
+                ray = Ray(eye.point, eye.vector + xcomp + ycomp)
+                colour = self.rayColour(ray)
+                canvas.plot(x, y, *colour)
+
+    def rayColour(self, ray):
+        if self.recursionDepth > 3:
+            return (0, 0, 0)
+        try:
+            self.recursionDepth = self.recursionDepth + 1
+            intersections = [(o, o.intersectionTime(ray), s)
+                             for (o, s) in self.objects]
+            i = firstIntersection(intersections)
+            if i is None:
+                return (0, 0, 0)  # the background colour
+            else:
+                (o, t, s) = i
+                p = ray.pointAtTime(t)
+                return s.colourAt(self, ray, p, o.normalAt(p))
+        finally:
+            self.recursionDepth = self.recursionDepth - 1
+
+    def _lightIsVisible(self, l, p):
+        for (o, s) in self.objects:
+            t = o.intersectionTime(Ray(p, l - p))
+            if t is not None and t > EPSILON:
+                return False
+        return True
+
+    def visibleLights(self, p):
+        result = []
+        for l in self.lightPoints:
+            if self._lightIsVisible(l, p):
+                result.append(l)
+        return result
+
+
+def addColours(a, scale, b):
+    return (a[0] + scale * b[0],
+            a[1] + scale * b[1],
+            a[2] + scale * b[2])
+
+
+class SimpleSurface(object):
+
+    def __init__(self, **kwargs):
+        self.baseColour = kwargs.get('baseColour', (1, 1, 1))
+        self.specularCoefficient = kwargs.get('specularCoefficient', 0.2)
+        self.lambertCoefficient = kwargs.get('lambertCoefficient', 0.6)
+        self.ambientCoefficient = 1.0 - self.specularCoefficient - self.lambertCoefficient
+
+    def baseColourAt(self, p):
+        return self.baseColour
+
+    def colourAt(self, scene, ray, p, normal):
+        b = self.baseColourAt(p)
+
+        c = (0, 0, 0)
+        if self.specularCoefficient > 0:
+            reflectedRay = Ray(p, ray.vector.reflectThrough(normal))
+            reflectedColour = scene.rayColour(reflectedRay)
+            c = addColours(c, self.specularCoefficient, reflectedColour)
+
+        if self.lambertCoefficient > 0:
+            lambertAmount = 0
+            for lightPoint in scene.visibleLights(p):
+                contribution = (lightPoint - p).normalized().dot(normal)
+                if contribution > 0:
+                    lambertAmount = lambertAmount + contribution
+            lambertAmount = min(1, lambertAmount)
+            c = addColours(c, self.lambertCoefficient * lambertAmount, b)
+
+        if self.ambientCoefficient > 0:
+            c = addColours(c, self.ambientCoefficient, b)
+
+        return c
+
+
+class CheckerboardSurface(SimpleSurface):
+
+    def __init__(self, **kwargs):
+        SimpleSurface.__init__(self, **kwargs)
+        self.otherColour = kwargs.get('otherColour', (0, 0, 0))
+        self.checkSize = kwargs.get('checkSize', 1)
+
+    def baseColourAt(self, p):
+        v = p - Point.ZERO
+        v.scale(1.0 / self.checkSize)
+        if ((int(abs(v.x) + 0.5)
+             + int(abs(v.y) + 0.5)
+             + int(abs(v.z) + 0.5)) % 2):
+            return self.otherColour
+        else:
+            return self.baseColour
+
+
+def bench_raytrace(loops, width, height, filename):
+    range_it = range(loops)
+    t0 = pyperf.perf_counter()
+
+    for i in range_it:
+        canvas = Canvas(width, height)
+        s = Scene()
+        s.addLight(Point(30, 30, 10))
+        s.addLight(Point(-10, 100, 30))
+        s.lookAt(Point(0, 3, 0))
+        s.addObject(Sphere(Point(1, 3, -10), 2),
+                    SimpleSurface(baseColour=(1, 1, 0)))
+        for y in range(6):
+            s.addObject(Sphere(Point(-3 - y * 0.4, 2.3, -5), 0.4),
+                        SimpleSurface(baseColour=(y / 6.0, 1 - y / 6.0, 0.5)))
+        s.addObject(Halfspace(Point(0, 0, 0), Vector.UP),
+                    CheckerboardSurface())
+        s.render(canvas)
+
+    dt = pyperf.perf_counter() - t0
+
+    if filename:
+        canvas.write_ppm(filename)
+    return dt
+
+
+def add_cmdline_args(cmd, args):
+    cmd.append("--width=%s" % args.width)
+    cmd.append("--height=%s" % args.height)
+    if args.filename:
+        cmd.extend(("--filename", args.filename))
+
+
+if __name__ == "__main__":
+    runner = pyperf.Runner(add_cmdline_args=add_cmdline_args)
+    cmd = runner.argparser
+    cmd.add_argument("--width",
+                     type=int, default=DEFAULT_WIDTH,
+                     help="Image width (default: %s)" % DEFAULT_WIDTH)
+    cmd.add_argument("--height",
+                     type=int, default=DEFAULT_HEIGHT,
+                     help="Image height (default: %s)" % DEFAULT_HEIGHT)
+    cmd.add_argument("--filename", metavar="FILENAME.PPM",
+                     help="Output filename of the PPM picture")
+
+    args = runner.parse_args()
+    runner.metadata['description'] = "Simple raytracer"
+    runner.metadata['raytrace_width'] = args.width
+    runner.metadata['raytrace_height'] = args.height
+
+    runner.bench_time_func('raytrace', bench_raytrace,
+                           args.width, args.height,
+                           args.filename)
